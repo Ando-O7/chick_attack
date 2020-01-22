d74e6d85f3f4161326d43d150cb6663c23f4dcea
ando07 <ando.masahito@gmail.com>
2020-01-22 13:20:49 +0900

    [add]shoot cannon ball script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 8f;
     public float moveableRange = 5.5f;
+    public float power = 1000f;
+    public GameObject cannonBall;
+    public Transform spawnPoint;
 
     // Update is called once per frame
     void Update()
@@ -15,5 +18,19 @@ public class PlayerController : MonoBehaviour
         transform.position = new Vector2(Mathf.Clamp(
             transform.position.x, -moveableRange, moveableRange),
             transform.position.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newBullet =
+            Instantiate(cannonBall, spawnPoint.position,
+            Quaternion.identity) as GameObject;
+        newBullet.GetComponent<Rigidbody2D>().AddForce(
+            Vector3.up * power);
     }
 }
