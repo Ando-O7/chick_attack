4781d7583dcca4722bbe5b7310e90bef44f4dbcb
ando07 <ando.masahito@gmail.com>
2020-01-23 13:44:11 +0900

    [modify]rename of class name

--- Assets/DestroyObj.cs
+++ Assets/DestroyObj.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DestroyObj : MonoBehaviour
 {
     public float deleteTime = 2.0f;
 
